b742270c9e87e71fae983063fa00d133c3308ce0
Rohan <rohan.sreenivasan@oracle.com>
2023-06-05 15:28:17 -0700

    basic transformations

--- grouping_basic.sql
+++ grouping_basic.sql
@@ -1,4 +1,5 @@
 
 SELECT country, COUNT(*) AS event_count
 FROM web_events
+INNER JOIN mobile_events ON web_events.user_id = mobile_events.user_id;
 GROUP BY country;
--- window_functions.sql
+++ window_functions.sql
@@ -1,3 +1,6 @@
 SELECT event_timestamp, event_type, user_id, 
        SUM(duration_seconds) OVER (PARTITION BY user_id ORDER BY event_timestamp) AS running_total
 FROM web_events;
+
+
+
