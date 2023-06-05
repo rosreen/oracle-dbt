bab79d372f9a3f2daf76dd62066ff06d73d183d9
Rohan <rohan.sreenivasan@oracle.com>
2023-06-05 15:31:27 -0700

    basic transformations

--- grouping_basic.sql
+++ grouping_basic.sql
@@ -1,5 +1,7 @@
 
-SELECT country, COUNT(*) AS event_count
+SELECT mobile_events.country, COUNT(*) AS event_count
 FROM web_events
-INNER JOIN mobile_events ON web_events.user_id = mobile_events.user_id;
-GROUP BY country;
+INNER JOIN mobile_events ON web_events.user_id = mobile_events.user_id
+GROUP BY mobile_events.country;
+
+
